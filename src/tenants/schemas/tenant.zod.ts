7fc571bbd6db0b7fb8ca62eca51e75e9f951a2b9
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-07-24 18:17:26 -0300

    fix (tenant): fixed tenant creation

--- src/tenants/schemas/tenant.zod.ts
+++ src/tenants/schemas/tenant.zod.ts
@@ -2,12 +2,13 @@ import { ROLES_VALUES } from 'src/constants/roles';
 import { z } from 'zod';
 
 export const ZodTenantSchema = z.object({
-  name: z.string(),
-  lastName: z.string(),
+  name: z.string().min(1),
+  lastName: z.string().min(1),
   email: z.string().email(),
   role: z.enum(ROLES_VALUES),
-  userName: z.string(),
-  password: z.string(),
-  tenantName: z.string(),
+  userName: z.string().min(1),
+  password: z.string().min(1),
+  companyName: z.string().min(1),
+  tenantName: z.string().optional(),
   image: z.string().optional(),
 });
